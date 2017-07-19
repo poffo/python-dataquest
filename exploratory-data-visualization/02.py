39720374ca6d7e8d26adf0c6b813cc8eba55fe88
poffo <robson.poffo@gmail.com>
2017-07-19 01:00:08 -0700

    Exploratory

--- exploratory-data-visualization/02.py
+++ exploratory-data-visualization/02.py
@@ -95,7 +95,81 @@ plt.show()
 print("\n"*3)
 
 
+unrate['MONTH'] = unrate['DATE'].dt.month
+
+fig = plt.figure(figsize=(6,3))
+
+x_values_1948_t = unrate["MONTH"][:12]
+y_values_1948_t = unrate["VALUE"][:12]
+x_values_1949_t = unrate["MONTH"][12:24]
+y_values_1949_t = unrate["VALUE"][12:24]
+
+#plt.xlabel("Month")
+#plt.ylabel("Inemployment Rate")
+#plt.title("Monthly Unemployment Trends, 1948/49")
+
+plt.plot(x_values_1948_t, y_values_1948_t, c='red')
+plt.plot(x_values_1949_t, y_values_1949_t, c='blue')
+plt.show()
 
 
+#################################
+print("\n"*3)
+
+fig = plt.figure(figsize=(10,6))
+
+def plotMultipleLines(yearNumber, colorYear):
+    x_values_year_t = unrate["MONTH"][(12*yearNumber):(12*(yearNumber+1))]
+    y_values_year_t = unrate["VALUE"][(12*yearNumber):(12*(yearNumber+1))]
+
+    plt.plot(x_values_year_t, y_values_year_t, color=colorYear)
+
+plotMultipleLines(0, 'red')
+plotMultipleLines(1, 'blue')
+plotMultipleLines(2, 'green')
+plotMultipleLines(3, 'orange')
+plotMultipleLines(4, 'black')
+
+plt.show()
+
+
+
+#################################
+print("\n"*3)
+
+
+# Modify the code from the last screen that overlaid 5 plots to include a legend. Use the year value for each line chart as the label.
+# E.g. the plot of 1948 data that uses "red" for the line color should be labeled "1948" in the legend.
+# Place the legend in the "upper left" corner of the plot.
+# Display the plot using plt.show().
+
+
+unrate['YEAR'] = unrate['DATE'].dt.year
+
+
+fig = plt.figure(figsize=(10,6))
+
+def plotMultipleLines(yearNumber, colorYear):
+    x_values_year_t = unrate["MONTH"][(12*yearNumber):(12*(yearNumber+1))]
+    y_values_year_t = unrate["VALUE"][(12*yearNumber):(12*(yearNumber+1))]
+
+    plt.plot(x_values_year_t, y_values_year_t, color=colorYear, label=unrate['YEAR'][12*yearNumber])
+
+plotMultipleLines(0, 'red')
+plotMultipleLines(1, 'blue')
+plotMultipleLines(2, 'green')
+plotMultipleLines(3, 'orange')
+plotMultipleLines(4, 'black')
+
+plt.xlabel("Month, Integer")
+plt.ylabel("Unemployment Rate, Percent")
+plt.title("Monthly Unemployment Trends, 1948-1952")
+plt.legend(loc='upper left')
+
+plt.show()
+
+
+#################################
+print("\n"*3)
 
 
